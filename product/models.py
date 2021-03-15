86b0fe1103ac9e0897a07b3e172502533ef61ac1
mustafabayarr <bayarmustafa151@gmail.com>
2021-03-15 20:03:59 +0300

    added admin panel and admin category

--- product/models.py
+++ product/models.py
@@ -16,7 +16,7 @@ class Category(models.Model):
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
-def __str__(self):
-    return  self.title
+    def __str__(self):
+        return  self.title
 
 
