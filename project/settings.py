4f8273b9ab3faabc726b3017c547006efe21b852
mustafabayarr <bayarmustafa151@gmail.com>
2021-03-15 19:38:37 +0300

    added db category table

--- project/settings.py
+++ project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'home.apps.HomeConfig',
+    'product.apps.ProductConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
