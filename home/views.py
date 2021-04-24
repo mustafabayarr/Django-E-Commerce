5523b647d6da58504d051cecbc9646dd9a9920d1
mustafabayarr <bayarmustafa151@gmail.com>
2021-04-24 23:52:18 +0300

    implemented contact,about us and references pages on frontend

--- home/views.py
+++ home/views.py
@@ -9,3 +9,18 @@ def index(request):
     setting = Setting.objects.get(pk=1)
     context = {'setting' : setting}
     return render(request,'index.html',context)
+
+def aboutUs(request):
+    setting = Setting.objects.get(pk=1)
+    context = {'setting': setting}
+    return render(request, 'aboutus.html', context)
+
+def references(request):
+    setting = Setting.objects.get(pk=1)
+    context = {'setting': setting}
+    return render(request, 'references.html', context)
+
+def contact(request):
+    setting = Setting.objects.get(pk=1)
+    context = {'setting': setting}
+    return render(request, 'contact.html', context)
--- project/urls.py
+++ project/urls.py
@@ -18,8 +18,13 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from home import views
+
 urlpatterns = [
     path('', include('home.urls')),
+    path('aboutus/',views.aboutUs,name='aboutus'),
+    path('references/',views.references,name='references'),
+    path('contact/',views.contact,name='contact'),
     path('home/', include('home.urls')),
     path('product/', include('product.urls')),
     path('admin/', admin.site.urls),
