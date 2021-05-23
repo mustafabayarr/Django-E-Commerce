298db0b636659e90db03546b70d3cef54fa77f82
mustafabayarr <bayarmustafa151@gmail.com>
2021-05-23 23:57:02 +0300

    added update user profile

--- user/urls.py
+++ user/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     #path('addcomment/<int:id>',views.addcomment,name='addcomment')
+    path('update/',views.user_update,name="user_update"),
+    path('password/',views.change_password,name="change_password")
 ]
--- user/views.py
+++ user/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from home.models import UserProfile
 from product.models import Category
+from user.forms import UserUpdateForm, ProfileUpdateForm
 
 
 def index(request):
@@ -13,3 +14,26 @@ def index(request):
     context = { 'category': category,'profile':profile}
     return render(request,'user_profile.html',context)
 
+
+def user_update(request):
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST,instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST,request.FILES,instance=request.user.userprofile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            return redirect('/user')
+    else:
+        category = Category.objects.all()
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.userprofile)
+        context = {
+            'category':category,
+            'user_form':user_form,
+            'profile_form':profile_form
+        }
+        return render(request,'user_update.html',context)
+
+
+def change_password(request):
+    return None
