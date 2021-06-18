8c94ad5b64b502f4f601208e023e5c25c5228329
mustafabayarr <bayarmustafa151@gmail.com>
2021-06-18 21:30:23 +0300

    admin FAQ

--- home/admin.py
+++ home/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from home.models import Setting, ContactFormMessage, UserProfile
+from home.models import Setting, ContactFormMessage, UserProfile, FAQ
 
 admin.site.register(Setting)
 
@@ -14,3 +14,9 @@ admin.site.register(ContactFormMessage,ContactFormAdmin)
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ['user_name','image_tag','phone','address','city','country']
 admin.site.register(UserProfile,UserProfileAdmin)
+
+class FAQAdmin(admin.ModelAdmin):
+    list_display =['ordernumber','question','answer','status']
+    list_filter =['status']
+admin.site.register(FAQ,FAQAdmin)
+
